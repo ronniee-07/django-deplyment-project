df5347d6b31dcf7751c99bfa9fe6d3a5b0d776ae
ronnie-07 <ronak.solank07@gmail.com>
2019-09-07 20:40:41 +0530

    'Added_login_project'

---
+++ logged_in/basic_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from basic_app.models import userProfileInfo
+# Register your models here.
+
+admin.site.register(userProfileInfo)
---
+++ logged_in/basic_app/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from basic_app.models import userProfileInfo
+
+class userForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta():
+        model = User
+        fields = ('username','email','password')
+
+class userProfileInfoForm(forms.ModelForm):
+    class Meta():
+        model = userProfileInfo
+        fields = ('portfolio_site','profile_pic')
